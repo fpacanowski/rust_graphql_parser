bcb90805375d6516e38ac2190e5ba3061bb2d5c5
Filip Pacanowski <fpacanowski@gmail.com>
2023-11-08 10:51:59 +0100

    Initial content generated by bundle gem.

---
+++ lib/rust_graphql_parser.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "rust_graphql_parser/version"
+require_relative "rust_graphql_parser/rust_graphql_parser"
+
+module RustGraphqlParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
