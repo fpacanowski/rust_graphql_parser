39ee790248cdaac0a66e922dbd3f366c85444d39
Filip Pacanowski <fpacanowski@gmail.com>
2023-11-13 17:51:26 +0100

    fix spec

--- spec/rust_graphql_parser_spec.rb
+++ spec/rust_graphql_parser_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe RustGraphqlParser do
          :selection_set=>
           {:node_type=>:selection_set,
            :span=>[{:line=>1, :column=>28}, {:line=>1, :column=>36}],
-           :items=>[{:node_type=>:field, :name=>"value", :position=>{:line=>1, :column=>30}, :selection_set=>{:node_type=>:selection_set, :span=>[{:line=>1, :column=>30}, {:line=>1, :column=>30}], :items=>[]}}]},
+           :items=>[{:node_type=>:field, :name=>"value", :position=>{:line=>1, :column=>30}, :selection_set=>{:node_type=>:selection_set, :span=>[{:line=>1, :column=>30}, {:line=>1, :column=>30}], :items=>[]}, :arguments=>[]}]},
          :type_condition=>{:on=>"Foo"}}]}
-         )
+    )
   end
 end
