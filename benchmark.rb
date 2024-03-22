016301f2b309b7d8b1cfe49c84ac45d55ca3e541
Filip Pacanowski <fpacanowski@gmail.com>
2024-03-22 17:21:14 +0100

    benchmark script tweaks

--- benchmark.rb
+++ benchmark.rb
@@ -4,11 +4,18 @@ require 'graphql/c_parser'
 require 'graphql'
 
 source = File.read('negotiate.gql')
-Benchmark.ips do |x|
+GC.disable
+r = Benchmark.ips do |x|
   x.report('parse_raw') { RustGraphqlParser.parse_raw(source) }
   x.report('parse_ruby') { GraphQL.parse(source) }
   x.report('parse_and_translate') { RustGraphqlParser.translate(RustGraphqlParser.parse(source)) }
   x.report('parse') { RustGraphqlParser.parse(source) }
 
   x.compare!
+end
+
+r.data.each do |entry|
+  iterations = entry.fetch(:iterations)
+  time = entry.fetch(:microseconds)
+  puts "#{entry.fetch(:name)} = #{format("%.2f", time/iterations/1000)} ms"
 end
